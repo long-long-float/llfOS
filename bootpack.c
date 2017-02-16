49ef42bd27714039e76575664656d8695fbe1276
long-long-float <niinikazuki@yahoo.co.jp>
2017-02-16 18:25:34 +0900

    マウスの割り込みを処理できるようにした

--- bootpack.c
+++ bootpack.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 extern FIFO8 keybuf;
+extern FIFO8 mousebuf;
+
+void init_keyboard();
+void enable_mouse();
 
 void HariMain() {
   init_gdtidt();
@@ -30,20 +34,40 @@ void HariMain() {
   byte kbuf[32];
   fifo8_init(&keybuf, 32, kbuf);
 
+  byte mbuf[128];
+  fifo8_init(&mousebuf, 32, mbuf);
+
+  init_keyboard();
+  enable_mouse();
+
   while (1) {
     // keybuf.dataを読み取っている間に割り込みが来たら困るので
     io_cli();
 
-    if (fifo8_count(&keybuf) > 0) {
-      int data = fifo8_pop(&keybuf);
+    if (fifo8_count(&keybuf) > 0 || fifo8_count(&mousebuf) > 0) {
+      if (fifo8_count(&keybuf) > 0) {
+        // キーボードの処理
+        int data = fifo8_pop(&keybuf);
 
-      io_sti();
+        io_sti();
 
-      boxfill8(info->vram, info->screenx, COLOR_LIGHT_BLUE, 0, 0, 32 * 8 - 1, 15);
+        boxfill8(info->vram, info->screenx, COLOR_BLACK, 0, FONT_HEIGHT * 2, info->screenx, FONT_HEIGHT * 3);
 
-      char buf[20];
-      sprintf(buf, "keyboard: %02X", data);
-      putfonts8_asc(info->vram, info->screenx, COLOR_WHITE, 0, 0, buf);
+        char buf[20];
+        sprintf(buf, "keyboard: %02X", data);
+        putfonts8_asc(info->vram, info->screenx, COLOR_WHITE, 0, FONT_HEIGHT * 2, buf);
+      } else {
+        // マウスの処理
+        int data = fifo8_pop(&mousebuf);
+
+        io_sti();
+
+        boxfill8(info->vram, info->screenx, COLOR_BLACK, 0, FONT_HEIGHT * 3, info->screenx, FONT_HEIGHT * 4);
+
+        char buf[20];
+        sprintf(buf, "mouse: %02X", data);
+        putfonts8_asc(info->vram, info->screenx, COLOR_WHITE, 0, FONT_HEIGHT * 3, buf);
+      }
     } else {
       // STIの後に割り込みが来るとキー情報があるのにHLTしてしまうので一緒に実行する
       io_stihlt();
@@ -51,3 +75,24 @@ void HariMain() {
   }
 }
 
+void wait_KBC_sendready() {
+  while (1) {
+    if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+      break;
+    }
+  }
+}
+
+void init_keyboard() {
+  wait_KBC_sendready();
+  io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+  wait_KBC_sendready();
+  io_out8(PORT_KEYDAT, KBC_MODE);
+}
+
+void enable_mouse() {
+  wait_KBC_sendready();
+  io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+  wait_KBC_sendready();
+  io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+}
